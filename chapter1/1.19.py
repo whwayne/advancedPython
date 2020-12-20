c5aba7ec6acb5a730e8fc09c80d465c01b92ad33
whwayne <whwayne@gmail.com>
2020-12-19 19:57:32 -0600

    first commit

---
+++ chapter1/1.19.py
@@ -0,0 +1,18 @@
+import time
+
+# ticks = time.time()
+# print('Number of ticks till now', ticks)
+
+# time tuple
+# localtime = time.localtime(time.time())
+# print('current local time', localtime)
+
+# format time
+# localtime = time.asctime(time.localtime(time.time()))
+# print('format current time', localtime)
+
+# calendar
+# import calendar
+#
+# cal = calendar.month(2020, 12)
+# print('Here is the calendar\n', cal)
