0f18f2bca6022d071715307001436916c55e6a4f
whwayne <whwayne@gmail.com>
2020-12-20 20:05:33 -0600

    1.16 list, tuple and dict

---
+++ chapter1/1.16.py
@@ -0,0 +1,77 @@
+# List
+
+# list1 = ['physics', 'chemistry', 1997, 2000]
+# list2 = [1, 2, 3, 4, 5, 6, 7]
+# list3 = ["a", "b", "c", "d"]
+#
+# print('list1[0]', list1[0])
+# print ('list2[1:5]', list2[1:5])
+
+# list = ['physics', 'chemistry', 1997, 2000];
+#
+# print("Value available at index 2 : ")
+# print(list[2])
+# list[2] = 2001
+# print("New value available at index 2 : ")
+# print(list[2])
+# del list[2]
+# print(list)
+
+# print(['Hi']*4)
+# print([1,2,3] + [4,5,6])
+# print(len([1,2,3]))
+# print(3 in [1,2,3])
+# for x in [1,2,3]:
+#     print(x)
+
+#TUPLE
+
+# tup1 = ('physics', 'chemistry', 1997, 2000);
+# tup2 = (1, 2, 3, 4, 5, 6, 7);
+# tup3 = "a", "b", "c", "d";
+# tup4 = (50,)
+#
+# print("tup1[0]: ", tup1[0])
+# print("tup2[1:5]: ", tup2[1:5])
+
+
+# tup1 = (12, 34.56);
+# # tup2 = ('abc', 'xyz');
+# #
+# # # tup1[0] = 100
+# # tup3 = tup1 + tup2
+# # print(tup3)
+
+
+# tup = ('physics', 'chemistry', 1997, 2000);
+#
+# print(tup);
+# del tup;
+# print("After deleting tup : ")
+# print(tup)
+
+# print ('abc', -4.24e93, 18+6.6j, 'xyz')
+# # x, y = 1, 2
+# # print (x, y)
+
+#DICT
+# dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'};
+#
+# print("dict['Name']: ", dict['Name'])
+# print("dict['Age']: ", dict['Age'])
+#
+# dict['Age'] = 8
+# dict['School'] = 'Maharishi'
+# print(dict)
+#
+# del dict['Name']
+# print(dict)
+# dict.clear()
+# del dict
+# print(dict)
+
+# dict = {['Name']: 'Zara', 'Age': 7};
+# # print("dict['Name']: ", dict['Name'])
+
+
+
